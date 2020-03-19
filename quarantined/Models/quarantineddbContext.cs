33218a2d44dd2874214a06666259b493af6c1a53
wister <e@wister.mx>
2020-03-19 01:31:45 -0600

    Removing conection string

--- quarantined/Models/quarantineddbContext.cs
+++ quarantined/Models/quarantineddbContext.cs
@@ -23,8 +23,7 @@ namespace quarantined.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:quarantined.database.windows.net,1433;Initial Catalog=quarantineddb;Persist Security Info=False;User ID=quarantined;Password=Redbull123.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("DBSTRING");
             }
         }
 
