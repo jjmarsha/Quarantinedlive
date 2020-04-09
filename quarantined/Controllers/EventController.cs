a82322c79ec7859d72df7b7d8489664b1681a1b1
wister <etorres@bside.com.mx>
2020-04-08 22:46:43 -0500

    Event Api

---
+++ quarantined/Controllers/EventController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonQ.Mocks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using quarantined.Services;
+
+namespace quarantined.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        public readonly EventServiceAgent _EventServiceAgent;
+        public EventController()
+        {
+            _EventServiceAgent = new EventServiceAgent();
+        }
+        // GET: api/Event
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var allEvents = _EventServiceAgent.GetAll();
+            return Ok(allEvents);
+        }
+
+        // GET: api/Event/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(string id)
+        {
+            var allEvents = _EventServiceAgent.GetById(Guid.Parse(id));
+            return Ok(allEvents);
+        }
+
+        // POST: api/Event
+        [HttpPost]
+        public IActionResult Post([FromBody] EventBody value)
+        {
+            try
+            {
+                value.Id = Guid.NewGuid();
+                if (_EventServiceAgent.Save(value))
+                {
+                    return StatusCode(202,_EventServiceAgent);
+                }
+                else
+                {
+                    return BadRequest("Error on create event");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, "Panic!!!!!!!!");
+            }
+        }
+
+        // PUT: api/Event/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ quarantined/Services/EventServiceAgent.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonQ.Mocks;
+using Newtonsoft.Json;
+
+namespace quarantined.Services
+{
+    public class EventServiceAgent
+    {
+        public bool Save(EventBody userData)
+        {
+            try
+            {
+                //var filePath = @"/app/eventMocks.json"; only if run in dockers
+                var filePath = @"eventMocks.json";
+                var jsonData = System.IO.File.ReadAllText(filePath);
+                var userList = JsonConvert.DeserializeObject<List<EventBody>>(jsonData) ?? new List<EventBody>();
+                userList.Add(userData);
+                jsonData = JsonConvert.SerializeObject(userList);
+                System.IO.File.WriteAllText(filePath, jsonData);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+        public List<EventBody> GetAll()
+        {
+            try
+            {
+                //var filePath = @"/app/eventMocks.json"; only if run in dockers
+                var filePath = @"eventMocks.json";
+                var jsonData = System.IO.File.ReadAllText(filePath);
+                var userList = JsonConvert.DeserializeObject<List<EventBody>>(jsonData) ?? new List<EventBody>();
+                return userList;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+        public EventBody GetById(Guid id)
+        {
+            try
+            {
+                //var filePath = @"/app/eventMocks.json"; only if run in dockers
+                var filePath = @"eventMocks.json";
+                var jsonData = System.IO.File.ReadAllText(filePath);
+                var userList = JsonConvert.DeserializeObject<List<EventBody>>(jsonData) ?? new List<EventBody>();
+                var found = userList.FirstOrDefault(o => o.Id == id);
+                return found;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+    }
+}
