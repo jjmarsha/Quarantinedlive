4a0914c28f652940688748b11ac9c757aa2f0878
John <jjmarsha@usc.edu>
2020-04-14 17:42:28 -0700

    more stuff

--- webapp/src/pages/entryview.jsx
+++ webapp/src/pages/entryview.jsx
@@ -7,26 +7,39 @@ import Rising from "../static/88.jpg";
 import TrendingSection from "../components/navigation/trendingsection";
 import axios from "axios";
 import Loading from "../common/loading";
+import { ParseDateString, ParseTimeString } from "../lists/daterefs";
 
 class EntryView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       entry: undefined,
+      parsedDateString: "",
     };
   }
 
   componentDidMount() {
     const id = this.props.history.location.pathname.substring("/view/".length);
     axios
-    .get(`http://quarantined.azurewebsites.net/api/Event/${id}`)
-    .then(resp => {
-      this.setState({entry: resp.data})
-    })
+      .get(`http://quarantined.azurewebsites.net/api/Event/${id}`)
+      .then((resp) => {
+        this.setState({
+          entry: resp.data,
+          parsedDateString: this.ParseDate(resp.data),
+        });
+      });
   }
 
+  ParseDate = (entry) => {
+    const date = new Date(entry.time + " UTC");
+    if (entry.time)
+      return `${ParseDateString(date.toString())} ${ParseTimeString(
+        date.toString().split(" ")[4]
+      )}`;
+  };
+
   render() {
-    if(!this.state.entry) return <Loading/>
+    if (!this.state.entry) return <Loading />;
     return (
       <Page>
         <Navigation />
@@ -37,14 +50,23 @@ class EntryView extends React.Component {
           <Col md="12" className="mt-5">
             <h4>Title of the event</h4>
             <br />
-            <p>Name of host: </p>
-            <p>Wednesday, April 8 | 3:00 PM (PST)</p>
-            <p>Topic</p>
-            <p>Language</p>
-            <p>Description</p>
+            <p>Name of host: {this.state.entry.nameofhost}</p>
+            <p>Date: {this.state.parsedDateString}</p>
             <p>
-              {this.state.entry.description}
+              Topics:{" "}
+              {this.state.entry.topics
+                ? this.state.entry.topics.map((topic, key) => {
+                    return (
+                      <React.Fragment key={key}>
+                        {(key === 0 ? "" : ", ") + topic}
+                      </React.Fragment>
+                    );
+                  })
+                : null}
             </p>
+            <p>Language: {this.state.entry.language}</p>
+            <p>Description: </p>
+            <p>{this.state.entry.description}</p>
             <a href={this.state.entry.link}>Link to event</a>
           </Col>
         </Row>
